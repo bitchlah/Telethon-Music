3078094c6dc4bab007b71a7c9b7c2ae2d8ebc64d
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-08-01 23:12:08 +0700

    Update admins.py

--- ALBY/plugins/admins.py
+++ ALBY/plugins/admins.py
@@ -10,12 +10,12 @@ from telethon.tl.functions.messages import ExportChatInviteRequest
 @ALBY.on(events.callbackquery.CallbackQuery(data="admin"))
 async def _(event):
 
-    await event.edit(ADMIN_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
+    await event.edit(ADMIN_TEXT, buttons=[[Button.inline("« Kembali", data="help")]])
 
 @ALBY.on(events.callbackquery.CallbackQuery(data="play"))
 async def _(event):
 
-    await event.edit(PLAY_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
+    await event.edit(PLAY_TEXT, buttons=[[Button.inline("« Kembali", data="help")]])
 
 @ALBY.on(events.NewMessage(pattern="^[!?/]promote ?(.*)"))
 @is_admin
@@ -23,16 +23,16 @@ async def promote(event, perm):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_private:
-       await event.reply("This cmd is made to be used in groups, not in PM!")
+       await event.reply("Perintah ini dibuat untuk digunakan dalam grup, bukan di PM!")
        return
 
     if not perm.add_admins:
-        await event.reply("You are missing the following rights to use this command:CanAddAdmins!")
+        await event.reply("Perintah ini hanya bisa digunakan admin, dan ❌ Anda Bukan Admin Group!❌")
         return
     input_str = event.pattern_match.group(1)
     user = await event.get_reply_message()
     if not input_str and not user:
-        await event.reply("Reply to a user or give its username to promote him!")
+        await event.reply("Balas pesan pengguna atau berikan username pengguna untuk diangkat menjadi admin!")
         return
     sed = await ALBY(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
@@ -44,10 +44,10 @@ async def promote(event, perm):
                     pin_messages=True), rank="Admin"))
 
     if not input_str:
-        await event.reply(f"Successfully Promoted [{sed.user.first_name}](tg://user?id={user.sender_id}) in {event.chat.title}!")
+        await event.reply(f"Berhasil diangkat menjadi Admin [{sed.user.first_name}](tg://user?id={user.sender_id}) in {event.chat.title}!")
         return
 
-    await event.reply(f"Succesfully Promoted {input_str} in {event.chat.title}")
+    await event.reply(f"Berhasil diangkat menjadi Admin {input_str} in {event.chat.title}")
  
 @ALBY.on(events.NewMessage(pattern="^[!?/]demote ?(.*)"))
 @is_admin
@@ -55,15 +55,15 @@ async def promote(event, perm):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_private:
-       await event.reply("This cmd is made to be used in groups, not in PM!")
+       await event.reply("Perintah ini dibuat untuk digunakan dalam grup, bukan di PM!")
        return
     if not perm.add_admins:
-        await event.reply("You are missing the following rights to use this command:CanAddAdmins!")
+        await event.reply("Perintah ini hanya bisa digunakan admin, dan ❌ Anda Bukan Admin Group!❌")
         return
     input_str = event.pattern_match.group(1)
     user = await event.get_reply_message()
     if not input_str and not user:
-        await event.reply("Reply to a user or give its username to demote him!")
+        await event.reply("Balas pesan pengguna atau berikan username pengguna untuk menurunkan hak admin!")
         return
     sed = await ALBY(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
@@ -75,10 +75,10 @@ async def promote(event, perm):
                     pin_messages=None), rank="Not Admin"))
 
     if not input_str:
-        await event.reply(f"Successfully Demoted [{sed.user.first_name}](tg://user?id={user.sender_id}) in {event.chat.title}!")
+        await event.reply(f"Berhasil Diturunkan admin [{sed.user.first_name}](tg://user?id={user.sender_id}) in {event.chat.title}!")
         return
 
-    await event.reply(f"Succesfully Demoted {input_str} in {event.chat.title}")
+    await event.reply(f"Berhasil Diturunkan admin {input_str} in {event.chat.title}")
  
 
 @ALBY.on(events.NewMessage(pattern="^[!?/]invitelink"))
@@ -86,28 +86,28 @@ async def invitelink(event):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_private:
-       await event.reply("This cmd is made to be used in groups, not in PM!")
+       await event.reply("Perintah ini dibuat untuk digunakan dalam grup, bukan di PM!")
        return
     link = await ALBY(ExportChatInviteRequest(event.chat_id))
-    await event.reply(f"Group link of {event.chat.title} is [here]({link.link})", link_preview=False)
+    await event.reply(f"Link grup dari {event.chat.title} is [here]({link.link})", link_preview=False)
 
 ADMIN_TEXT = """
-**✘ A module from which admins of the chat can use!**
+**✘ Modul yang dapat digunakan oleh admin grup!**
 
-‣ `?promote` - To Promote a user in the chat.
-‣ `?demote` - To Demote a user in the chat.
-‣ `?invitelink` - To get invitelink of a chat.
-‣ `?end` - To End music streaming.
-‣ `?skip` - To Skip Tracks Going on.
-‣ `?pause` - To Pause streaming.
-‣ `?resume` - to Resume Streaming.
-‣ `?leavevc` - force The Userbot to leave Vc Chat (Sometimes Joined).
-‣ `?playlist` - to check playlists.
+‣ `?promote` - Untuk Mengangkat pengguna menjadi admin dalam grup.
+‣ `?demote` - Untuk Menurunkan pengguna dari hak admin dalam grup.
+‣ `?invitelink` - Untuk mendapatkan tautan undangan grup.
+‣ `?end` - Untuk Mengakhiri musik.
+‣ `?skip` - Untuk Melewati Trek Terjadi.
+‣ `?pause` - Untuk Menjeda musik.
+‣ `?resume` - Untuk melanjutkan musik.
+‣ `?leavevc` - memaksa Asisten untuk meninggalkan Vc Chat (Terkadang Bergabung).
+‣ `?playlist` - untuk memeriksa daftar putar.
 """
 
 PLAY_TEXT = """
-**✘ A module from which users of the chat can use!**
+**✘ Modul yang dapat digunakan oleh pengguna obrolan!**
 
-‣ `?play` - To Play Audio from Else Reply to audio file.
-‣ `?vplay` - To Stream Videos (HEROKU_MODE > Doesn't support).
+‣ `?play` - Untuk Memutar Musik atau Balas ke file audio.
+‣ `?vplay` - Untuk Memutar Video (HEROKU_MODE > Tidak mendukung).
 """
