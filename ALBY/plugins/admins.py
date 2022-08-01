ea44dc433a86f834123414f9325e4c14d8a2616e
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-08-01 15:36:59 +0700

    Update and rename Zaid/plugins/admins.py to ALBY/plugins/admins.py

--- ALBY/plugins/admins.py
+++ ALBY/plugins/admins.py
@@ -1,23 +1,23 @@
 from telethon import events, Button
-from Zaid import Zaid
-from Zaid.status import *
+from ALBY import ALBY
+from ALBY.status import *
 from Config import Config
 from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.functions.messages import ExportChatInviteRequest
 
-@Zaid.on(events.callbackquery.CallbackQuery(data="admin"))
+@ALBY.on(events.callbackquery.CallbackQuery(data="admin"))
 async def _(event):
 
     await event.edit(ADMIN_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
 
-@Zaid.on(events.callbackquery.CallbackQuery(data="play"))
+@ALBY.on(events.callbackquery.CallbackQuery(data="play"))
 async def _(event):
 
     await event.edit(PLAY_TEXT, buttons=[[Button.inline("« Bᴀᴄᴋ", data="help")]])
 
-@Zaid.on(events.NewMessage(pattern="^[!?/]promote ?(.*)"))
+@ALBY.on(events.NewMessage(pattern="^[!?/]promote ?(.*)"))
 @is_admin
 async def promote(event, perm):
     if Config.MANAGEMENT_MODE == "ENABLE":
@@ -34,7 +34,7 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("Reply to a user or give its username to promote him!")
         return
-    sed = await Zaid(GetFullUserRequest(id=user.sender_id or input_str))
+    sed = await ALBY(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=True,
@@ -49,7 +49,7 @@ async def promote(event, perm):
 
     await event.reply(f"Succesfully Promoted {input_str} in {event.chat.title}")
  
-@Zaid.on(events.NewMessage(pattern="^[!?/]demote ?(.*)"))
+@ALBY.on(events.NewMessage(pattern="^[!?/]demote ?(.*)"))
 @is_admin
 async def promote(event, perm):
     if Config.MANAGEMENT_MODE == "ENABLE":
@@ -65,7 +65,7 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("Reply to a user or give its username to demote him!")
         return
-    sed = await Zaid(GetFullUserRequest(id=user.sender_id or input_str))
+    sed = await ALBY(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=None,
@@ -81,14 +81,14 @@ async def promote(event, perm):
     await event.reply(f"Succesfully Demoted {input_str} in {event.chat.title}")
  
 
-@Zaid.on(events.NewMessage(pattern="^[!?/]invitelink"))
+@ALBY.on(events.NewMessage(pattern="^[!?/]invitelink"))
 async def invitelink(event):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_private:
        await event.reply("This cmd is made to be used in groups, not in PM!")
        return
-    link = await Zaid(ExportChatInviteRequest(event.chat_id))
+    link = await ALBY(ExportChatInviteRequest(event.chat_id))
     await event.reply(f"Group link of {event.chat.title} is [here]({link.link})", link_preview=False)
 
 ADMIN_TEXT = """
