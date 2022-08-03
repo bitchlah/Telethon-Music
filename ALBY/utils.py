e5179dff94fc4958965d2cab4a22136ff934608e
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-08-04 05:38:26 +0700

    Update and rename Zaid/utils.py to ALBY/utils.py

--- ALBY/utils.py
+++ ALBY/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Zaid/plugins/{plugin_name}.py")
-    name = "Zaid.plugins.{}".format(plugin_name)
+    path = Path(f"ALBY/plugins/{plugin_name}.py")
+    name = "ALBY.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Zaid.plugins." + plugin_name] = load
+    sys.modules["ALBY.plugins." + plugin_name] = load
     print("Bot has Started " + plugin_name)
